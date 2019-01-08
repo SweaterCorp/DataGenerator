3490221b610e1bc698d384501f2271984450b033
Yuriy Aksenov <Yura290610@yandex.ru>
2019-01-08 18:47:06 +0700

    [Add] General Colors Tons

---
+++ ColorsExcelParser/Colors/ColorsMatching.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ColorsExcelParser.Colors
+{
+  public class ColorsMatching
+  {
+    public ColorsGroups Autumn { get; set; } = new ColorsGroups();
+    public ColorsGroups Spring { get; set; } = new ColorsGroups();
+    public ColorsGroups Summer { get; set; } = new ColorsGroups();
+    public ColorsGroups Winter { get; set; } = new ColorsGroups();
+  }
+}
