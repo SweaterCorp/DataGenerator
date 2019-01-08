093bf56f46b105790a4aa14ba92ab882152d8ba1
Yuriy Aksenov <Yura290610@yandex.ru>
2019-01-08 18:45:24 +0700

    [Add] String Extension Replace Sequence

---
+++ DataParser/StringExtenstion.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace DataParser
+{
+  public static class StringExtenstion
+  {
+    public static string ReplaceSequence(this string str, IList<char> oldValues, string newValue)
+    {
+      var builder = new StringBuilder();
+      foreach (var @char in str)
+        if (oldValues.Contains(@char)) builder.Append(@char);
+        else builder.Append(newValue);
+      return builder.ToString();
+    }
+  }
+}
