91a9dbf35a150736d5ccabd761d408778ccc16b7
Yuriy Aksenov <Yura290610@yandex.ru>
2019-01-08 18:47:39 +0700

    [Add] Debag staff to check it up

---
+++ ColorsExcelParser/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace ColorsExcelParser
+{
+  internal class Program
+  {
+    private static void Main(string[] args)
+    {
+      Console.WriteLine("Hello World!");
+      var path = "E:\\Projects\\Zebra\\Project\\Theory\\SuitableColors.xlsx";
+      var savePath = "E:\\Projects\\Zebra\\Project\\Data\\ColorsMatching.json";
+      var colors = new ColorsParser().ReadColors(path);
+      var json = JsonConvert.SerializeObject(colors);
+
+      using (var sw = new StreamWriter(savePath, false, Encoding.UTF8))
+      {
+        sw.WriteLine(json);
+      }
+    }
+  }
+}
