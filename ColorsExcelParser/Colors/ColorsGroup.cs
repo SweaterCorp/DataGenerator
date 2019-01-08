cdb2378ec3b37b3b613c2a290f06629f5da97da6
Yuriy Aksenov <Yura290610@yandex.ru>
2019-01-08 18:46:27 +0700

    [Add] ColorsGroup which contain good bad base colors

---
+++ ColorsExcelParser/Colors/ColorsGroup.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace ColorsExcelParser.Colors
+{
+  public class ColorsGroup
+  {
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public List<ColorType> BaseColors { get; set; }
+    public List<string> GoodColors { get; set; } = new List<string>();
+    public List<string> BadColors { get; set; } = new List<string>();
+
+    public ColorsGroup() { }
+
+    public ColorsGroup(int id, string name, List<ColorType> colors)
+    {
+      Id = id;
+      Name = name;
+      BaseColors = colors;
+    }
+  }
+}
