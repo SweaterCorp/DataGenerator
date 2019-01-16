89a643b5307ea477c0c3ddffc51861b9251bc594
Yuriy Aksenov <Yura290610@yandex.ru>
2019-01-16 14:22:33 +0700

    [Add] Check main programm

--- MainConsole/Program.cs
+++ MainConsole/Program.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using CommonLibraries.Infrastructures;
+using System;
+using System.Linq;
+using CommonLibraries.CommonTypes;
+using CommonLibraries.Resources;
+using CommonLibraries.Resources.DeserializerTypes;
 
 namespace MainConsole
 {
@@ -6,7 +11,25 @@ namespace MainConsole
   {
     static void Main(string[] args)
     {
-      Console.WriteLine("Hello World!");
+      
+
+      ContentPath content = new ContentPath();
+      var resource = new ResourceHandler();
+      var colorsMatching = resource.ReadeResourceFile<LamodaColorsDeserializer>(content.LamodaColors);
+
+      Console.WriteLine(ServerColor.GetDifference(new ServerColor("000000"), new ServerColor("000000")));
+      Console.WriteLine(ServerColor.GetDifference(new ServerColor("ffffff"), new ServerColor("ffffff")));
+      Console.WriteLine(ServerColor.GetDifference(new ServerColor("dfbd93"), new ServerColor("dfbd93")));
+
+      var baseColors = BaseColorType.AsList();
+
+      ServerColor beige = new ServerColor("dfbd93");
+      foreach (var baseColorType in baseColors)
+      {
+        var otherColor = new ServerColor(baseColorType.Hex);
+        var diff = ServerColor.GetDifference(beige, otherColor);
+        Console.WriteLine($"Base Color: {baseColorType.Name}:      {diff} | ({otherColor.ToRgb().R}, {otherColor.ToRgb().G}, {otherColor.ToRgb().B}) | ({beige.ToRgb().R}, {beige.ToRgb().G}, {beige.ToRgb().B})");
+      }
     }
   }
 }
